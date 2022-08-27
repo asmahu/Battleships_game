be3e6499aa354e04e70432cccfe9f75bc13bd9ca
asmahu <asma.hussaini2018@gmail.com>
2022-08-27 01:48:19 +0000

    adapt board class

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+import sys
+
+scores = {'CPU': 0, 'player': 0}
+
+
+# Board class adopted and modified from the CI's battleship tutorial
+class Board:
+
+    """
+    Main board class. to set board size, the number of ships,
+     the player's name and the player board or CPU
+     contains methods to add ships, guesses and to print the board
+    """
